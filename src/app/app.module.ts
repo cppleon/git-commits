c8019c4029897eed422e0ec6bca2c0eaa23dd34b
Aslan Gurbanov <cppleon@gmail.com>
2019-12-26 16:35:35 +0400

    commits page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CommitsComponent } from './components/commits/commits.component';
+import { KeysPipe } from './pipes/keys.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CommitsComponent
+    CommitsComponent,
+    KeysPipe
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     HttpClientModule,
     AppRoutingModule
   ],
--- src/app/components/commits/commits.component.ts
+++ src/app/components/commits/commits.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { GithubService } from 'src/app/services/github.service';
 import { Commit } from 'src/app/classes/Commit';
 import { Branch } from 'src/app/classes/Branch';
+import { GitCommitsParams } from 'src/app/classes/GitCommitsParams';
+
+import { groupBy } from 'lodash';
 
 @Component({
   selector: 'app-commits',
@@ -10,20 +13,20 @@ import { Branch } from 'src/app/classes/Branch';
 })
 export class CommitsComponent implements OnInit {
 
-  readonly owner: string = 'googleforgames';
-  readonly repo: string = 'open-match';
+  readonly owner: string = 'cppleon';
+  readonly repo: string = 'git-commits';
 
   commits: Commit[] = [];
   branches: Branch[] = [];
+
+  grouppedCommits: { [key: string]: Commit[]; } = {};
 
   selectedBranch: Branch;
 
   constructor(private _githubService: GithubService) { }
 
   ngOnInit() {
-    this._githubService.getCommits(this.owner, this.repo).subscribe(res => {
-      this.commits = res;
-    });
+    this.getCommits('master');
 
     this._githubService.getBranches(this.owner, this.repo).subscribe(res => {
       this.branches = res;
@@ -36,8 +39,22 @@ export class CommitsComponent implements OnInit {
     });
   }
 
-  isSelected({ name }) {
-    return this.selectedBranch.name === name;
+  getCommits(sha: string, until?: string) {
+    const parameters: GitCommitsParams = { sha };
+
+    if (until) {
+      parameters.until = new Date(Date.parse(until) - 1).toISOString();
+    }
+
+    this._githubService.getCommits(this.owner, this.repo, parameters).subscribe(res => {
+      this.commits = res;
+
+      this.grouppedCommits = groupBy(this.commits, (commit: any) => new Date(commit.commit.author.date).toLocaleDateString());
+    });
+  }
+
+  onChangeBranch(branch: Branch) {
+    this.getCommits(branch.commit.sha);
   }
 
 }
